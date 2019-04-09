7ba936f03933dec7929bdf5ac9ae42b9b7bd5386
Cividati <rubens.cividati.teles@gmail.com>
2019-04-09 16:49:40 -0300

    SimpleProject

---
+++ namelessGame/src/namelessgame/NamelessGame.java
@@ -0,0 +1,16 @@
+/*
+ * (assunto da aula)
+ * data: 
+ * Rubens Cividati
+ */
+
+package namelessgame;
+
+
+public class NamelessGame {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
