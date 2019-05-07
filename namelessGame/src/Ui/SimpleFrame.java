e1ab73561c1c0e6c3bb8d961382a9faa7703ddfc
Gabriel Claret <gclaret@outlook.com>
2019-05-07 01:38:40 -0300

    New Frames
    Ui updates: created menu, new game, load game and some buttons actions.

--- namelessGame/src/Ui/SimpleFrame.java
+++ namelessGame/src/Ui/SimpleFrame.java
@@ -126,10 +126,10 @@ public class SimpleFrame extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
